dec439154f83bde693ce0bccbb0cd512d3bca738
Recep <49814995+Recepurkun@users.noreply.github.com>
2024-06-07 17:52:20 +0300

    some fix

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import errorMsg from "../assets/404.svg";
+import { Button } from "./Styled";
 
-const Error = ({ message }) => {
+const Error = ({ message, scrollToTop }) => {
   return (
-    <div>
-      <img src={errorMsg} alt="Error" className="error-image" />
-      <p>{message}</p>
-      <button> Yukarıya çık</button>
+    <div className="text-center mt-2 p-5">
+      <p className="fs-2 text-danger m-0">{message}</p>
+      <img src={errorMsg} alt="Error" className="img-fluid" />
+      <Button $primary={false} onClick={scrollToTop}>
+        <i className="fa-solid fa-arrow-up mx-2"></i> Yukarıya çık
+      </Button>
     </div>
   );
 };
